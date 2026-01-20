d1d49506ebd080e66362dc019a1fb9a939b2bdc4
rvsony761 <104378805+rvsony761@users.noreply.github.com>
2026-01-20 22:14:11 +0530

    Add files via upload

---
+++ Dbmodels.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Course.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentMobile { get; set; }
+
+    }
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Duration { get; set; }
+        public string CourseType { get; set; }
+        public decimal CourseFees { get; set; }
+    }
+
+    public class Payment_detail
+    {
+        [Key]
+        public int PaymentId { get; set; }
+        public string PaymentMethodName { get; set; }
+        public decimal Payment_Discunt { get; set; }
+    }
+    public class Enrollment_detail
+    {
+        [Key]
+        public int EnrollmentId { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public int PaymentId { get; set; }
+        public DateTime? Enrollment_Date { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Discount { get; set; }
+        public decimal GST { get; set; }
+        public decimal Total_Payable { get; set; }
+
+        [ForeignKey("PaymentId")] 
+        public virtual Payment_detail Paymentdetailinfo { get; set; }
+
+        [ForeignKey("CourseId")]
+        public virtual Course Courseinfo { get; set; }
+
+        [ForeignKey("StudentId")]
+        public virtual Student Studentinfo { get; set; }
+
+        [NotMapped]
+        public List<SelectListItem> StudentList { get; set; }
+        
+        [NotMapped]
+        public List<SelectListItem> CourseList { get; set; }
+        
+        [NotMapped]
+        public List<SelectListItem> PaymentMethodList { get; set; }
+        
+        [NotMapped]
+        public List<int> SelectedCoursesIds { get; set; }
+    }
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext():base("cn")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment_detail> Enrollment_details { get; set; }
+        public DbSet<Payment_detail> Payment_details { get; set; }
+
+    }
+    public class spreport1
+    {
+        public string CourseName { get; set; }
+        public string Duration { get; set; }
+        public int Total_Students { get; set; }
+        public decimal Total_Payable { get; set; }
+
+    }
+}
---
+++ HomeControllerCustom.cs
@@ -0,0 +1,93 @@
+﻿using Student_Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Course.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext db=new ApplicationDbContext();
+
+        private void Binddropdown(Enrollment_detail record)
+        {
+            string qry = "Select cast(StudentId as varchar(10))[Value],StudentName[Text] from Students";
+            record.StudentList = db.Database.SqlQuery<SelectListItem>(qry).ToList();
+            string qry2 = "Select cast(CourseId as varchar(10))[Value],[CourseName][Text] from Courses";
+            record.CourseList = db.Database.SqlQuery<SelectListItem>(qry2).ToList();
+            record.SelectedCoursesIds = new List<int>();
+            string qry3 = "Select cast(PaymentId as varchar(10))[Value],PaymentMethodName[Text] from Payment_detail";
+            record.PaymentMethodList = db.Database.SqlQuery<SelectListItem>(qry3).ToList();
+        }
+        public ActionResult Create()
+        {
+            Enrollment_detail record=new Enrollment_detail();
+            Binddropdown(record);
+            return View(record);
+        }
+        [HttpPost]
+        public ActionResult Create(Enrollment_detail record)
+        {
+            
+            if(!ModelState.IsValid)
+            {
+                Binddropdown(record);
+                return View(record);
+            }
+            int results=0;
+            foreach (var item in record.SelectedCoursesIds)
+            {
+                Enrollment_detail obj = new Enrollment_detail();
+                var student=db.Students.Where(x=>x.StudentId==record.StudentId).FirstOrDefault();
+                var course=db.Courses.Where(x=>x.CourseId==item).FirstOrDefault();
+                var payment = db.Payment_details.Where(x => x.PaymentId == record.PaymentId).FirstOrDefault();
+                if(course.CourseType=="Online")
+                {
+                    obj.Amount = course.CourseFees+(course.CourseFees*5)/100;
+                }
+                else
+                {
+                    obj.Amount = course.CourseFees;    
+                }
+                obj.GST = (obj.Amount * 12) / 100;
+                obj.Discount = ((obj.Amount + obj.GST) * payment.Payment_Discunt) / 100;
+                obj.Total_Payable = (obj.Amount + obj.GST) - obj.Discount;
+                obj.StudentId=record.StudentId;
+                obj.CourseId = item;
+                obj.PaymentId = record.PaymentId;
+                obj.Enrollment_Date= record.Enrollment_Date;
+                db.Enrollment_details.Add(obj);    
+            }
+            results = db.SaveChanges();
+            if(results>0)
+            {
+                TempData["message"]="Data Saves Successfully";
+            }
+            return RedirectToAction("Create");
+        }
+
+        public ActionResult Report1()
+        {
+            List<spreport1> report=new List<spreport1>();
+
+            return View(report);
+        }
+        [HttpPost]
+        public ActionResult Report1(DateTime StartDate,DateTime EndDate)
+        {
+            List<spreport1> report = new List<spreport1>();
+            report=db.Database.SqlQuery<spreport1>("Exec spreport1 @StartDate,@EndDate",new SqlParameter("@StartDate", StartDate), new SqlParameter("@EndDate", EndDate)).ToList();
+            return View(report);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
